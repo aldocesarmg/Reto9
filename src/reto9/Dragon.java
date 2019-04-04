daa0a47697b1c7348c4a32f9ee1d6c38b8156970
aldocesarmg <aldocesarmg@gmail.com>
2019-04-04 09:12:10 -0600

    Creacion de clase Dragon

---
+++ src/reto9/Dragon.java
@@ -0,0 +1,12 @@
+
+package reto9;
+
+public class Dragon {
+    public String nombre;
+    private int nivelFuego;
+    private int nivelVida;
+    
+    public void SecuestrarUnicornios(String nombre){
+        
+    }
+}
---
+++ src/reto9/Reto9.java
@@ -0,0 +1,10 @@
+
+package reto9;
+
+public class Reto9 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
